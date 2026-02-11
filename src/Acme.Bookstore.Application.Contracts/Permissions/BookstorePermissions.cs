2ab7190066c95b069320124436401480cee687ab
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2026-02-11 15:46:28 +0700

    Permissions

--- src/Acme.Bookstore.Application.Contracts/Permissions/BookstorePermissions.cs
+++ src/Acme.Bookstore.Application.Contracts/Permissions/BookstorePermissions.cs
@@ -5,7 +5,16 @@ public static class BookstorePermissions
     public const string GroupName = "Bookstore";
 
 
-    
+
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
+
+    // *** ADDED a NEW NESTED CLASS ***
+    public static class Books
+    {
+        public const string Default = GroupName + ".Books";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
 }

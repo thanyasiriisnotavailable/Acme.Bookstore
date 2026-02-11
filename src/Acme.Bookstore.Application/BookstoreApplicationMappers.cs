1ead85dc29bc3cfb16aa519a6fc9844637663ec4
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2026-02-11 13:34:59 +0700

    Create DTO

--- src/Acme.Bookstore.Application/BookstoreApplicationMappers.cs
+++ src/Acme.Bookstore.Application/BookstoreApplicationMappers.cs
@@ -1,3 +1,5 @@
+using Acme.Bookstore.Books;
+using Acme.BookStore.Books;
 using Riok.Mapperly.Abstractions;
 using Volo.Abp.Mapperly;
 
@@ -13,3 +15,18 @@ namespace Acme.Bookstore;
  *    public override partial void Map(BookDto source, CreateUpdateBookDto destination);
  * }
  */
+[Mapper]
+public partial class BookToBookDtoMapper : MapperBase<Book, BookDto>
+{
+    public override partial BookDto Map(Book source);
+
+    public override partial void Map(Book source, BookDto destination);
+}
+
+[Mapper]
+public partial class CreateUpdateBookDtoToBookMapper : MapperBase<CreateUpdateBookDto, Book>
+{
+    public override partial Book Map(CreateUpdateBookDto source);
+
+    public override partial void Map(CreateUpdateBookDto source, Book destination);
+}

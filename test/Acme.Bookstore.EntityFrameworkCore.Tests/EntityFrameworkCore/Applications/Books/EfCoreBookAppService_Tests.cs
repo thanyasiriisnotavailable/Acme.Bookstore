d9636a1c85a5339c64630ee9d2b94bb27ea02134
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2026-02-11 15:30:54 +0700

    Test Projects in the Solution

---
+++ test/Acme.Bookstore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/EfCoreBookAppService_Tests.cs
@@ -0,0 +1,12 @@
+﻿using Acme.Bookstore;
+using Acme.Bookstore.EntityFrameworkCore;
+using Acme.BookStore.Books;
+using Xunit;
+
+namespace Acme.BookStore.EntityFrameworkCore.Applications.Books;
+
+[Collection(BookstoreTestConsts.CollectionDefinitionName)]
+public class EfCoreBookAppService_Tests : BookAppService_Tests<BookstoreEntityFrameworkCoreTestModule>
+{
+
+}

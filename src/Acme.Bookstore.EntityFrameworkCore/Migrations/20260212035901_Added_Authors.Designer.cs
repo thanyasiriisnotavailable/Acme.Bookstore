14264bd0acf7b03602569ecf957c5b0ebe8c36c2
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2026-02-12 11:03:09 +0700

    Create new database migration

--- src/Acme.Bookstore.EntityFrameworkCore/Migrations/20260212035901_Added_Authors.Designer.cs
+++ src/Acme.Bookstore.EntityFrameworkCore/Migrations/20260212035901_Added_Authors.Designer.cs
@@ -4,6 +4,7 @@ using Acme.Bookstore.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Acme.Bookstore.Migrations
 {
     [DbContext(typeof(BookstoreDbContext))]
-    partial class BookstoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260212035901_Added_Authors")]
+    partial class Added_Authors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

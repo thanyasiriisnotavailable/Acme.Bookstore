309a05ed13fd9ef1710694d292cf0fa829c1bb5d
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2026-02-12 10:43:25 +0700

    Provide domain service & repository

--- src/Acme.Bookstore.Domain.Shared/BookstoreDomainErrorCodes.cs
+++ src/Acme.Bookstore.Domain.Shared/BookstoreDomainErrorCodes.cs
@@ -2,5 +2,5 @@
 
 public static class BookstoreDomainErrorCodes
 {
-    /* You can add your business exception error codes here, as constants */
+    public const string AuthorAlreadyExists = "Bookstore:00001";
 }
---
+++ src/Acme.Bookstore.Domain/Authors/AuthorAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿using Acme.Bookstore;
+using Volo.Abp;
+
+namespace Acme.BookStore.Authors;
+
+public class AuthorAlreadyExistsException : BusinessException
+{
+    public AuthorAlreadyExistsException(string name)
+        : base(BookstoreDomainErrorCodes.AuthorAlreadyExists)
+    {
+        WithData("name", name);
+    }
+}
